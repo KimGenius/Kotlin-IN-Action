bd835ceb3eb506d021382a8d999f93b444afd777
GeniusK <geniusk@rinc.kr>
2018-04-24 00:05:39 +0900

    type check 추가

--- src/ch1/P44.kt
+++ src/ch1/P44.kt
@@ -1,13 +1,13 @@
 package ch1
 
 class P44 {
-    fun renderPersonList(persons: Collection<Person>) =
-            createHTML().table {
-                for (person in persons) {
-                    tr {
-                        td {+person.name}
-                        td {person.age}
-                    }
-                }
-            }
+//    fun renderPersonList(persons: Collection<Person>) =
+//            createHTML().table {
+//                for (person in persons) {
+//                    tr {
+//                        td {+person.name}
+//                        td {person.age}
+//                    }
+//                }
+//            }
 }
---
+++ src/ch1/P57.kt
@@ -0,0 +1,8 @@
+package ch1
+
+fun main(args: Array<String>) {
+  val a = 1
+  val b = "1"
+  println(a is Int)
+  println(b is String)
+}
