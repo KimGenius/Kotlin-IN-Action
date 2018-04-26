17cf948e188a11098d8d00091766d0089ce9fd08
GeniusK <geniusk@rinc.kr>
2018-04-26 18:13:40 +0900

    Add P61

---
+++ src/ch2/P61.kt
@@ -0,0 +1,18 @@
+package ch2
+
+fun returnTest(): String {
+  return "GeniusK"
+}
+
+fun test() {
+
+}
+
+fun simpleTest() = "GeniusK"
+
+// 코틀린 기본 함수 형태
+/*
+  fun 함수이름(매개변수 이름: 매개변수 타입) : 반환 타입 {
+
+  }
+ */
