266908c9abdc6648430cfad0c712566d4a6a87a6
GeniusK <geniusk@rinc.kr>
2018-06-21 00:51:21 +0900

    변수

---
+++ src/ch2/P64.java
@@ -0,0 +1,14 @@
+package ch2;
+
+import java.util.ArrayList;
+
+public class P64 {
+    public static void main(String[] args) {
+        String testVar = "test";
+        testVar = "This is not test!";
+        final String testVal = "testVar";
+//        testVal = "This is not test!";
+        final ArrayList<String> testArr = new ArrayList<String>();
+        testArr.add("test1");
+    }
+}
---
+++ src/ch2/P64.kt
@@ -0,0 +1,13 @@
+package ch2
+
+fun main(args: Array<String>) {
+  var testVar = "test"
+  testVar = "This is not test!"
+  var testType: String = "Test"
+  val testVal = "testVar"
+//        testVal = "This is not test!"
+  val testArr = arrayListOf("Kotlin")
+  testArr.add("test1")
+  var testWhat = 1
+//  testWhat = "asdf"
+}
